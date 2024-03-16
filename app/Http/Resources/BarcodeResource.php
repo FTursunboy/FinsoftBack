f29d17e594bad3560be14be48db441287939f90d
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2024-03-16 17:09:51 +0500

    sdcjsdc

--- app/Http/Resources/BarcodeResource.php
+++ app/Http/Resources/BarcodeResource.php
@@ -5,9 +5,13 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/** @mixin \App\Models\Barcode */
 class BarcodeResource extends JsonResource
 {
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
     public function toArray(Request $request): array
     {
         return [
