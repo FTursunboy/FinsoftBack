d86c12628215b219cb075b87695aa9d9a72030a8
FTursunboy <tfaiziev04@gmail.com>
2024-03-30 16:00:52 +0500

    changes

--- app/Http/Middleware/ApiRequestLockMiddleware.php
+++ app/Http/Middleware/ApiRequestLockMiddleware.php
@@ -3,6 +3,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class ApiRequestLockMiddleware
 {
@@ -14,10 +15,12 @@ class ApiRequestLockMiddleware
             $cacheKey = 'last_post_time_' . ($request->user()?->id);
             $lastPostTime = Cache::get($cacheKey);
 
+            Log::info($lastPostTime);
             if ($lastPostTime && now()->diffInSeconds($lastPostTime) < 5) {
                 return response()->json(['error' => 'Too many attempts'], 429);
             }
 
+            Log::error($request->ip());
             Cache::put($cacheKey, now(), 5);
         }
 
