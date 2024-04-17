767512f66bb1543d6826be7710b5698e1bdcb08e
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2024-04-17 14:21:40 +0500

    fddf

--- app/Http/Requests/Api/Organization/FilterRequest.php
+++ app/Http/Requests/Api/Organization/FilterRequest.php
@@ -3,26 +3,16 @@
 namespace App\Http\Requests\Api\Organization;
 
 use App\Models\Organization;
-use ErrorException;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Str;
-use ReflectionClass;
-use ReflectionMethod;
 
 class FilterRequest extends FormRequest
 {
     public function rules(): array
     {
-        $model = $this->getModel();
-
-        $fillableFields = $this->getFillable($model);
-
         return [
                 'search' => 'string|nullable|max:20',
                 'itemsPerPage' => 'integer|nullable',
-                'orderBy' => 'nullable|in:id,deleted_at,currency.name,organization.name,' . implode(',', $fillableFields),
+                'orderBy' => 'nullable|in:id,name,address,description,INN,director_id,chief_accountant_id',
                 'sort' => 'in:asc,desc',
                 'filterData' => 'nullable|array',
             ];
@@ -31,15 +21,5 @@ class FilterRequest extends FormRequest
     public function authorize(): bool
     {
         return true;
-    }
-
-    private function getFillable($model) :array
-    {
-        return $model->getFillable();
-    }
-
-    private function getModel()
-    {
-        return Organization::class;
     }
 }
