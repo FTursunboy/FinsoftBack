afed20484a5aab320e1b31ba7ade8091d71537f5
FTursunboy <tfaiziev04@gmail.com>
2024-04-18 16:20:21 +0500

    changes

--- app/Models/CashStore.php
+++ app/Models/CashStore.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class CashStore extends Model
 {
@@ -25,5 +26,9 @@ class CashStore extends Model
         'senderCashRegister_id',
         'employee_id',
         'balanceKey_id',
+        'operation_type',
+        'type'
     ];
+
+
 }
--- app/Repositories/CashStore/ClientPaymentRepository.php
+++ app/Repositories/CashStore/ClientPaymentRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\CashStore;
 
 use App\DTO\ClientPaymentDTO;
 use App\Models\CashStore;
+use App\Models\OrderDocument;
 use App\Repositories\Contracts\CashStore\CashStoreRepositoryInterface;
 
 class ClientPaymentRepository implements CashStoreRepositoryInterface
@@ -14,6 +15,33 @@ class ClientPaymentRepository implements CashStoreRepositoryInterface
 
     public function clientPayment(ClientPaymentDTO $dto)
     {
-        dd($dto);
+
+        $this->model::create([
+            'doc_number' => $this->orderUniqueNumber(),
+            'date' => $dto->date,
+            'organization_id' => $dto->organization_id,
+            'cashRegister_id' => $dto->cashRegister_id,
+            'sum' => $dto->sum,
+            'counterparty_id' => $dto->counterparty_id,
+            'counterparty_agreement_id' => $dto->counterparty_agreement_id,
+            'basis' => $dto->basis,
+            'comment' => $dto->comment,
+            'operation_type' => $dto->operation_type,
+            'type' => $dto->type
+        ]);
     }
+
+    public function orderUniqueNumber(): string
+    {
+        $lastRecord = CashStore::query()->orderBy('doc_number', 'desc')->first();
+
+        if (!$lastRecord) {
+            $lastNumber = 1;
+        } else {
+            $lastNumber = (int)$lastRecord->doc_number + 1;
+        }
+
+        return str_pad($lastNumber, 7, '0', STR_PAD_LEFT);
+    }
+
 }
