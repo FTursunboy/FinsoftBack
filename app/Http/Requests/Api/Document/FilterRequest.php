9b08f65f604b95c7f225d094a8731c34c184d09c
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2024-04-19 14:22:15 +0500

    dfdf

--- app/Http/Requests/Api/Document/FilterRequest.php
+++ app/Http/Requests/Api/Document/FilterRequest.php
@@ -15,7 +15,7 @@ class FilterRequest extends FormRequest
         return [
                 'search' => 'string|nullable|max:20',
                 'itemsPerPage' => 'integer|nullable',
-                'orderBy' => 'nullable|in:id,deleted_at,currency.name,organization.name,' . implode(',', $fillableFields),
+                'orderBy' => 'nullable|in:id,deleted_at,currency.name,organization.name,author.name,storage.name,counterparty.name,' . implode(',', $fillableFields),
                 'sort' => 'in:asc,desc',
                 'filterData' => 'nullable|array',
             ];
