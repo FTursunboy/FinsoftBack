11615693e763b0ba5f1e6ed2d1ca10c09b8c4623
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2024-04-25 16:35:35 +0500

    fddf

--- app/Repositories/DocumentRepository.php
+++ app/Repositories/DocumentRepository.php
@@ -37,13 +37,12 @@ class DocumentRepository implements DocumentRepositoryInterface
     {
         $filteredParams = $this->model::filter($data);
 
-
         $query = $this->model::query()->where('status_id', $status);
 
         $query = $this->search($query, $filteredParams);
 
         $query = $this->filter($query, $filteredParams);
-
+        
         $query = $this->sort($filteredParams, $query, ['counterparty', 'organization', 'storage', 'author', 'counterpartyAgreement', 'currency']);
 
         return $query->paginate($filteredParams['itemsPerPage']);
