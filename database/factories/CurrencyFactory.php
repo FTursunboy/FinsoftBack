0e21539a755ee576d7e419bb1ccf5750e4bd770d
FTursunboy <tfaiziev04@gmail.com>
2024-05-04 14:27:23 +0500

    factory changes

--- database/factories/CurrencyFactory.php
+++ database/factories/CurrencyFactory.php
@@ -5,15 +5,18 @@ namespace Database\Factories;
 use App\Models\Currency;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-public function definition(): array
+class CurrencyFactory extends Factory
 {
-    $currencySuffixes = ['tjs', 'usd', 'rub', 'eur', 'gbp', 'jpy', 'cad', 'aud', 'chf', 'cny', 'sek', 'nok', 'dkk', 'sgd', 'nzd', 'hkd', 'krw', 'inr', 'brl', 'mxn', 'try', 'zar', 'thb', 'idr', 'php', 'myr', 'rub', 'huf', 'czk', 'pln'];
+    public function definition(): array
+    {
+        $currencySuffixes = ['tjs', 'usd', 'rub', 'eur', 'gbp', 'jpy', 'cad', 'aud', 'chf', 'cny', 'sek', 'nok', 'dkk', 'sgd', 'nzd', 'hkd', 'krw', 'inr', 'brl', 'mxn', 'try', 'zar', 'thb', 'idr', 'php', 'myr', 'rub', 'huf', 'czk', 'pln'];
 
 
-    return [
-        'name'  => $currencySuffixes[array_rand($currencySuffixes)],
-        'symbol_code'  => $currencySuffixes[array_rand($currencySuffixes)],
-        'digital_code' => fake()->numberBetween(100, 999)
-    ];
+        return [
+            'name' => $currencySuffixes[array_rand($currencySuffixes)],
+            'symbol_code' => $currencySuffixes[array_rand($currencySuffixes)],
+            'digital_code' => fake()->numberBetween(100, 999)
+        ];
 
+    }
 }
