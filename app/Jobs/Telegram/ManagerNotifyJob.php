c9748d34b91f81ccc9fdeee471a4cc43e120e86f
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2024-05-10 17:13:12 +0500

    f

---
+++ app/Jobs/Telegram/ManagerNotifyJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs\Telegram;
+
+use App\Models\OrderDocument;
+use App\Notifications\Telegram\ManagerNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Notification;
+
+class ManagerNotifyJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private OrderDocument $document;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(OrderDocument $document)
+    {
+        $this->document = $document;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Notification::send(auth()->user(), new ManagerNotification($this->document));
+    }
+}
