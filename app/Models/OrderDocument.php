c3635becd4be841019aa8412191c078651b3154c
FTursunboy <tfaiziev04@gmail.com>
2024-05-21 15:36:34 +0500

    fsa

--- app/Models/OrderDocument.php
+++ app/Models/OrderDocument.php
@@ -9,22 +9,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
-class OrderDocument extends Model  implements Documentable
+class OrderDocument extends DocumentModel  implements Documentable
 {
     protected $fillable = ['doc_number','date','counterparty_id','counterparty_agreement_id','organization_id','shipping_date',
                 'order_status_id','author_id','comment','currency_id','summa', 'order_type_id'];
 
-    protected $keyType = 'string';
-
-    public $incrementing = false;
-
-    public static function boot() {
-        parent::boot();
-
-        static::creating(function ($model) {
-            $model->id = Str::uuid();
-        });
-    }
 
     public function counterparty(): BelongsTo
     {
@@ -63,7 +52,7 @@ class OrderDocument extends Model  implements Documentable
 
     public static function filter(array $data): array
     {
-        $filteredData =  [
+        $filteredData = [
             'search' => $data['search'] ?? '',
             'sort' => $data['orderBy'] ?? null,
             'direction' => $data['sort'] ?? 'asc',
@@ -94,10 +83,5 @@ class OrderDocument extends Model  implements Documentable
         }
 
         return $filteredData;
-    }
-
-    public function history(): HasMany
-    {
-        return $this->hasMany(DocumentHistory::class, 'document_id');
     }
 }
