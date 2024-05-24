9557b8b0a8482550d649e337a131201b2c7b1745
FTursunboy <tfaiziev04@gmail.com>
2024-05-24 11:14:38 +0500

    changes

--- app/Enums/DocumentTypes.php
+++ app/Enums/DocumentTypes.php
@@ -2,24 +2,21 @@
 
 namespace App\Enums;
 
-enum DocumentTypes :string
+enum DocumentTypes: string
 {
+    case Purchase = 'Покупка';
 
-        case Purchase = 'Покупка';
+    case ReturnProvider = 'Возврат поставщику';
 
-        case ReturnProvider = 'Возврат поставщику';
+    case SaleToClient = 'Продажа клиенту';
 
-        case SaleToClient = 'Продажа клиенту';
+    case ReturnClient = 'Возврат от клиента';
 
-        case ReturnClient = 'Возврат от клиента';
+    case ClientOrder = 'Заказ клиента';
 
-        case ClientOrder = 'Заказ клиента';
+    case ProviderOrder = 'Заказ поставщику';
 
-        case ProviderOrder = 'Заказ поставщику';
+    case Inventory = 'Инвентаризация';
 
-        case Inventory = 'Инвентаризация';
-
-        case Movement = 'Перемещение';
-
-
+    case Movement = 'Перемещение';
 }
